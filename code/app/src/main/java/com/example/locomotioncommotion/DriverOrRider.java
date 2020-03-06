3b93975ed6e593c7ed73c51ec175cc7bd0188385
Wilson Quon <wquon@ualberta.ca>
2020-03-06 16:02:21 -0700

    added button click with intents.

--- code/app/src/main/java/com/example/locomotioncommotion/DriverOrRider.java
+++ code/app/src/main/java/com/example/locomotioncommotion/DriverOrRider.java
@@ -1,4 +1,21 @@
 package com.example.locomotioncommotion;
 
-class DriverOrRider {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class DriverOrRider extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rider_or_driver);
+    }
+
+    public void riderClick(View view){
+        Intent intent = new Intent(this, Rider.class);
+        startActivity(intent);
+    }
+
 }
--- code/app/src/main/java/com/example/locomotioncommotion/Rider.java
+++ code/app/src/main/java/com/example/locomotioncommotion/Rider.java
@@ -1,4 +1,21 @@
 package com.example.locomotioncommotion;
 
-public class Rider {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Rider extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rider);
+    }
+
+    public void create(View view){
+        Intent intent = new Intent(this, createRequest.class);
+        startActivity(intent);
+    }
+
 }
