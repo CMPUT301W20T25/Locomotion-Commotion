f00241a22ba2d314bc84ddaaaa6c8a43898ac4e4
alex schafer <aschafer@ualberta.ca>
2020-03-08 19:13:43 -0600

    Added files properly and added java files for the activities

---
+++ code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
@@ -0,0 +1,14 @@
+package com.example.locomotioncommotion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverRequestsAll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_requests_all);
+    }
+}
