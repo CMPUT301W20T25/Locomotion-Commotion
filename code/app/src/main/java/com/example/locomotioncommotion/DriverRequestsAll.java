74b13d84fda707b0eabee0a4ee2a39a4c3575010
alex schafer <aschafer@ualberta.ca>
2020-03-08 21:35:54 -0600

    added button functionality to DriverRequestsAll

--- code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
+++ code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
@@ -2,7 +2,9 @@ package com.example.locomotioncommotion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DriverRequestsAll extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DriverRequestsAll extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_requests_all);
+    }
+
+    public void goToRequestFinder(View view) {
+        Intent intent = new Intent(this,DriverRequestsAll.class);
+        startActivity(intent);
+
     }
 }
