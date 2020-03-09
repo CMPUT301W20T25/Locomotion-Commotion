967f5e9a001476faaaa722d9a1737731cb990e14
Wilson Quon <wquon@ualberta.ca>
2020-03-08 23:51:39 -0600

    fixed intent and button is now working

--- code/app/src/main/java/com/example/locomotioncommotion/DriverOrRider.java
+++ code/app/src/main/java/com/example/locomotioncommotion/DriverOrRider.java
@@ -3,14 +3,25 @@ package com.example.locomotioncommotion;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-class DriverOrRider extends AppCompatActivity {
+public class DriverOrRider extends AppCompatActivity {
+    private Button riderSide;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rider_or_driver);
+
+        riderSide = findViewById(R.id.rider);
+        riderSide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                riderClick(v);
+            }
+        });
     }
 
     public void riderClick(View view){
