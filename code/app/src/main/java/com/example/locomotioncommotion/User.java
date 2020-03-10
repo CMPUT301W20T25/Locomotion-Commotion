b926f21f891a46b0ee3f235e856e4112cbcea831
SaimonH <snhumphr@ualberta.ca>
2020-03-10 14:26:11 -0600

    Adds in getters/setters for the User class.

--- code/app/src/main/java/com/example/locomotioncommotion/User.java
+++ code/app/src/main/java/com/example/locomotioncommotion/User.java
@@ -17,4 +17,53 @@ public class User {
         this.rider = null;
     }
 
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    //TODO: investigate whether this is a massive security vulnerability
+    public String getPassWord(){
+        return this.passWord;
+    }
+
+    public void setPassWord(String passWord){
+        this.passWord = passWord;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public Rider getRider(){
+        return this.rider;
+    }
+
+    public void setRider(Rider rider){
+        this.rider = rider;
+    }
+
+    public Driver getDriver(){
+        return this.driver;
+    }
+
+    public void setDriver(Driver driver){
+        this.driver = driver;
+    }
+
 }
