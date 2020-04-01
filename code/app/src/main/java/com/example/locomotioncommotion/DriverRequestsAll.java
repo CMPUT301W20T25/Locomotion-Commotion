6966a890c95a6ce07bc1ceb5fa650fca3c260225
Alex Nordstrom <6267244+HyperEpsilon@users.noreply.github.com>
2020-04-01 16:55:35 -0600

    Cheated and used firebase

--- code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
+++ code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
@@ -1,20 +1,43 @@
 package com.example.locomotioncommotion;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class DriverRequestsAll extends AppCompatActivity {
     public static final String REQUEST_MANAGE_MESSAGE = "com.example.locomotioncommotion.MANAGE_REQUEST";
     private Button scanCode;
+    private FirebaseFirestore db;
+    ListView requestList;
+    ArrayAdapter<Request> requestAdapter;
+    ArrayList<Request> requestDataList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_requests_all);
+
+        requestList = findViewById(R.id.driver_requests_list);
+
+        requestDataList = new ArrayList<>();
+
+        requestAdapter = new RequestList(this, requestDataList);
+        requestList.setAdapter(requestAdapter);
 
 
 
@@ -25,6 +48,27 @@ public class DriverRequestsAll extends AppCompatActivity {
                 scanQR(v);
             }
         });
+
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("requests");
+
+        collectionReference
+                .whereEqualTo("status", "Accepted")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        requestDataList.clear();
+                        for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                            Request request = doc.toObject(Request.class);
+                            if (request.getDriverUsername().equals(CurrentUser.getInstance().getUser().getUserName())) {
+                                request.setFirebaseID(doc.getId());
+                                requestDataList.add(request);
+                            }
+                        }
+                        requestAdapter.notifyDataSetChanged();
+                    }
+                });
+
     }
 
     public void goToRequestFinder(View view) {
