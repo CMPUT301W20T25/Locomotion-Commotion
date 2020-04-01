d047d005c85dc6280923b86adc1fada743b3f205
alex schafer <aschafer@ualberta.ca>
2020-03-31 21:47:08 -0600

    Locations are now shown in ViewDriverRequest.java

--- code/app/src/main/java/com/example/locomotioncommotion/ViewDriverRequest.java
+++ code/app/src/main/java/com/example/locomotioncommotion/ViewDriverRequest.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ViewDriverRequest extends AppCompatActivity {
+    Request request;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_driver_request);
+
+        Intent intent = getIntent();
+        request = (Request) intent.getExtras().getSerializable(RequestFinderList.REQUEST_MANAGE_MESSAGE);
+
+        TextView startLocation = findViewById(R.id.view_driver_request_start_location);
+        TextView endLocation = findViewById(R.id.view_driver_request_end_location);
+
+        startLocation.setText(request.getStartLocation().getName());
+        endLocation.setText(request.getEndLocation().getName());
+
+
     }
 
     public void clickUserName(View view) {
