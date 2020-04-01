8f3675dd9da1edfc23d5a3db5747e3ae8094cb5c
Wilson Quon <wquon@ualberta.ca>
2020-03-31 18:13:26 -0600

    scanner is in driver side now

--- code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
+++ code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DriverRequestsAll extends AppCompatActivity {
-
+    private Button scanCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +22,24 @@ public class DriverRequestsAll extends AppCompatActivity {
          //   TextView pickUp = findViewById(R.id.driver_requests_all_dropoff);
          //   pickUp.setText(user.getDriver().getCurrentRequest().getStartLocation());
         //}
+
+        scanCode = findViewById(R.id.driver_requests_all_scan);
+        scanCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                scanQR(v);
+            }
+        });
     }
 
     public void goToRequestFinder(View view) {
         Intent intent = new Intent(this,RequestFinderList.class);
         startActivity(intent);
 
+    }
+    public void scanQR(View view){
+        Intent intent = new Intent(this, QRCodeScanner.class);
+        startActivity(intent);
     }
 
     public void backButton(View view) {
