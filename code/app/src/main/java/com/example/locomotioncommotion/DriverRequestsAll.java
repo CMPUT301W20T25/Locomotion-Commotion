3b663a888892570497929e8d6b2ef64fec381fcc
alex schafer <aschafer@ualberta.ca>
2020-04-02 21:36:51 -0600

    clicking on active request now brings up ViewDriverRequest

--- code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
+++ code/app/src/main/java/com/example/locomotioncommotion/DriverRequestsAll.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -69,6 +70,13 @@ public class DriverRequestsAll extends AppCompatActivity {
                     }
                 });
 
+
+        requestList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                viewRequest(view);
+            }
+        });
     }
 
     public void goToRequestFinder(View view) {
